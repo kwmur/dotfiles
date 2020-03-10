e4aefa82f6adc590751e92e711bc33f33dccaed2
kwmur <alarm.clock.website@gmail.com>
2020-03-10 21:33:22 +0900

    make windows Symbolic links to file

--- make_link_win_7-10.bat
+++ make_link_win_7-10.bat
@@ -18,5 +18,11 @@ mklink /d "%HOME%\.emacs.d" "%DOTFILES%\.emacs.d"
 rem symbolic link .vim
 mklink /d "%HOME%\.vim" "%DOTFILES%\.vim"
 
+rem symbolic link files
+for %%f in (.bash_profile .bashrc .bashrc.cygwin .ccl-init.lisp .clisprc.lisp .ghci .gemrc .tigrc) do (
+  rem symbolic link %%f
+  mklink "%HOME%\%%f" "%DOTFILES%\%%f"
+)
+
 endlocal
 
