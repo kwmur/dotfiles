817768eed311fe0a341fb97c846c5fe3bcdc132c
kwmur <alarm.clock.website@gmail.com>
2020-03-10 22:09:56 +0900

    update windows Symbolic links to file

--- make_link_cygwin_linux_osx_unix.sh
+++ make_link_cygwin_linux_osx_unix.sh
@@ -70,28 +70,25 @@ backup_and_copy(){
 backup_and_copy .vimrc .gvimrc .gitconfig
 
 # common link
-make_link .bash_profile .bashrc .ccl-init.lisp .clisprc.lisp .ghci .gemrc .tigrc
 
-uname -a | grep -i cygwin > /dev/null
+# TODO: $OSTYPE is better ?
+uname -a | grep -i cygwin > /dev/null # Cygwin?
 if [ $? -eq 0 ]; then
-  # Cygwin
-  make_link .bashrc.cygwin
-  # .vim .emacs.d
-  if [ ! -e ~/.vim -a ! -e ~/.emacs.d ]; then
-    # OS
-    case `uname` in
-      CYGWIN_NT-5.1) # Windows XP ?
-        echo "winXP"
+  case `uname` in # OS
+    CYGWIN_NT-5.1) # Windows XP ?
+      echo "winXP"
+      if [ ! -e ~/.vim -a ! -e ~/.emacs.d ]; then
         #./make_link_win_xp.bat # TODO: test
-        ;;
-      CYGWIN_NT-10.0) # Windows 10
-        echo "win10"
-        #./make_link_win_7.bat # TODO: test
-        ;;
-    esac
-  fi
+      fi
+      ;;
+    CYGWIN_NT-10.0) # Windows 10
+      echo "win10"
+      #./make_link_win_7-10.bat # TODO: test
+      ;;
+  esac
 else
   # Unix, Linux, Mac OS X
+  make_link .bash_profile .bashrc .ccl-init.lisp .clisprc.lisp .ghci .gemrc .tigrc
   make_link .emacs.d .vim
 
   # Ubuntu
